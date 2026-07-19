19f067622d0c27430301da64c5ab07d9887219eb
r <120068330+bladeclickers@users.noreply.github.com>
2026-07-19 21:19:00 +1000

    add boosting

--- car.gd
+++ car.gd
@@ -9,8 +9,10 @@ extends VehicleBody3D
 @onready var bws = [bl, br]
 
 @onready var cam = $Camera3D
+@onready var speedometer = $"../Label"
+@onready var boost_gauge = $"../ProgressBar"
 
-const SPEED = 150
+var SPEED = 150
 const REVERSE = -50
 const BRAKE = 5
 const STEER = 0.5
@@ -18,15 +20,31 @@ const MIN_FOV = 70
 
 var maxSteer = 30
 var braking = false
+var boost = 100
+var boosting = false
 
 func _process(delta: float) -> void:
-	var speed = linear_velocity.dot(-global_transform.basis.z)
-	$"../Label".text = "Speed: " + str(round(-speed*10)/10)
+	if boost < 100 and not boosting:
+		boost += 5*delta
+	
+	if Input.is_action_pressed("boost") and boost > 0:
+		boosting = true
+		SPEED = 500
+		boost -= 25*delta
+	else:
+		boosting = false
+		SPEED = 150
+	
+	boost_gauge.value = boost
+	
+	var speed = -linear_velocity.dot(-global_transform.basis.z)
+	speedometer.text = "Speed: " + str(round(speed*10)/10)
 	
 	if Input.is_action_pressed("backward"):
 		braking = true
 		for wheel in bws:
 			if speed > 0:
+				wheel.engine_force = 0
 				wheel.brake = BRAKE
 			else:
 				wheel.engine_force = REVERSE
@@ -49,10 +67,7 @@ func _process(delta: float) -> void:
 	else:
 		for wheel in fws:
 			wheel.steering = move_toward(wheel.steering, 0, 0.05)
-	if Input.is_action_pressed("Restart"):
-		get_tree().change_scene_to_file("res://3d.tscn")
+	#if Input.is_action_pressed("Restart"):
+	#	get_tree().change_scene_to_file("res://3d.tscn")
 	
-	if speed < 0:
-		cam.fov = MIN_FOV - (speed/4)
-	else:
-		cam.fov = max(cam.fov - 0.2, MIN_FOV)
+	cam.fov = MIN_FOV - (-speed/3)
