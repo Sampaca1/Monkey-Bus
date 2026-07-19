63ec1acd950436db96b8b7f7a584bbfb905eb0db
r <120068330+bladeclickers@users.noreply.github.com>
2026-07-19 15:54:12 +1000

    add reversing

--- car.gd
+++ car.gd
@@ -8,26 +8,35 @@ extends VehicleBody3D
 @onready var fws = [fl, fr]
 @onready var bws = [bl, br]
 
+@onready var cam = $Camera3D
+
 const SPEED = 100
 const BRAKE = 5
 const STEER = 0.5
 
 var maxSteer = 30
 
+var braking = false
+
 func _process(delta: float) -> void:
+	if Input.is_action_pressed("backward"):
+		braking = true
+		var speed = linear_velocity.dot(-global_transform.basis.z)
+		for wheel in bws:
+			if speed > 0.1:
+				wheel.brake = BRAKE
+			else:
+				wheel.engine_force = -SPEED
+	else:
+		braking = false
+		for wheel in bws:
+			wheel.brake = 0
 	if Input.is_action_pressed("forward"):
 		for wheel in bws:
 			wheel.engine_force = SPEED
-	else:
+	elif not braking:
 		for wheel in bws:
 			wheel.engine_force = 0
-	if Input.is_action_pressed("backward"):
-		for wheel in bws:
-			wheel.brake = BRAKE
-		
-	else:
-		for wheel in bws:
-			wheel.brake = 0
 	if Input.is_action_pressed("left"):
 		for wheel in fws:
 			wheel.steering = clamp(wheel.steering + STEER * delta, -deg_to_rad(maxSteer), deg_to_rad(maxSteer))
