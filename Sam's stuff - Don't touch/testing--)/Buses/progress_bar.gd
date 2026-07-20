1b76ed29da8215ce5e825072201f14e67d87eeb2
r <120068330+bladeclickers@users.noreply.github.com>
2026-07-20 19:51:54 +1000

    make boost bar look better

--- Sam's stuff - Don't touch/testing--)/Buses/progress_bar.gd
+++ Sam's stuff - Don't touch/testing--)/Buses/progress_bar.gd
@@ -4,3 +4,7 @@ func _ready():
 	var sb = StyleBoxFlat.new()
 	add_theme_stylebox_override("fill", sb)
 	sb.bg_color = Color("00ff00")
+	sb.corner_radius_bottom_left = 5
+	sb.corner_radius_top_left = 5
+	sb.corner_radius_bottom_right = 5
+	sb.corner_radius_top_right = 5
