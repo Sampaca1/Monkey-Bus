035c87e966d3a8df07b832599c33fec3253aea01
BladeSkilled MC <bladeskilledmc@gmail.com>
2026-07-20 15:15:19 +1000

    add boost + handbrake to bus 2

--- Sam's stuff - Don't touch/testing--)/Buses/Bus 2.gd
+++ Sam's stuff - Don't touch/testing--)/Buses/Bus 2.gd
@@ -7,15 +7,19 @@ extends VehicleBody3D
 
 @onready var speedometer = $"Label"
 @onready var area = $Area3D
+@onready var cam = $"3rd Person"
+@onready var boost_gauge = $ProgressBar
 
 @onready var fws = [fl, fr]
 @onready var bws = [bl, br]
 
-const SPEED = 3000
+var SPEED = 3000
 const BRAKE = 200
 const STEER = 0.5
 
 var maxSteer = 30
+var boost = 100
+var boosting = false
 
 var isGettingDriven := false
 
@@ -36,7 +40,22 @@ func _process(delta: float) -> void:
 	if isGettingDriven:
 		var speed = -linear_velocity.dot(-global_transform.basis.z)
 		speedometer.text = "Speed: " + str(round(speed*10)/10)
+		
+		if boost < 100 and not boosting:
+			boost += 4*delta
+		
+		if Input.is_action_pressed("boost") and boost > 0:
+			boosting = true
+			SPEED = 9000
+			boost -= 32*delta
+		else:
+			boosting = false
+			SPEED = 3000
+		
+		boost_gauge.value = boost
+		
 		Bus.inputAndMove(fws, bws, SPEED, BRAKE, STEER, maxSteer, linear_velocity, rotation, delta, global_transform)
+		cam.fov = clamp(80 + speed*2, 80, 150)
 
 func _bodyEnteredDriverSeat(body: Node3D) -> void:
 	if body is CharacterBody3D:
