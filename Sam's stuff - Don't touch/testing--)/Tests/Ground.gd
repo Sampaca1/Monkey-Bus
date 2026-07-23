856210fcf23b217808a59bd043ad44ab056714ce
Sampaca1 <164956227+Sampaca1@users.noreply.github.com>
2026-07-23 11:06:58 +1000

    added test 5

--- Sam's stuff - Don't touch/testing--)/Tests/Ground.gd
+++ Sam's stuff - Don't touch/testing--)/Tests/Ground.gd
@@ -1,6 +1,6 @@
 extends MeshInstance3D
 
-@onready var bus: VehicleBody3D = $"../Bus 1"
+@onready var bus: VehicleBody3D = $"../Bus"
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
